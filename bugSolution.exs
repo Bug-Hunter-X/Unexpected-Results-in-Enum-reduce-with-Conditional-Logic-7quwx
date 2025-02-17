48746f1f3c0a980d060e5d48801ae22cafa326d2
BugHunter-X <askzenai@gmail.com>
2025-02-17 17:16:05 +0100

    Add bugSolution.exs file

---
+++ bugSolution.exs
@@ -0,0 +1,11 @@
+```elixir
+list = [1, 2, 3, 4, 5]
+
+Enum.reduce(list, 0, fn x, acc ->
+  if x > 3 do
+    acc + x
+  else
+    acc # Correctly handles the 'else' case by passing the acc as is
+  end
+end)
+```
